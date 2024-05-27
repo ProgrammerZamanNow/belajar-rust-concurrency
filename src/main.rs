195e12f2f5bc1f2be53ff68760d9d17abdf78e2a
khannedy <echo.khannedy@gmail.com>
2024-05-27 16:04:03 +0700

    Add thread creation test
    This commit adds a new test function `test_create_thread` within the tests module in `main.rs`. This function tests thread creation and execution, along with printing a counter and sleeping for a specific duration.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
 fn main() {
     println!("Hello, world!");
 }
+
+#[cfg(test)]
+mod tests {
+
+    use std::thread;
+    use std::time::Duration;
+
+    #[test]
+    fn test_create_thread(){
+        thread::spawn(|| {
+            for i in 0..=5 {
+                println!("Counter : {}", i);
+                thread::sleep(Duration::from_secs(1));
+            }
+        });
+
+        println!("Application done");
+        thread::sleep(Duration::from_secs(7));
+    }
+
+}
