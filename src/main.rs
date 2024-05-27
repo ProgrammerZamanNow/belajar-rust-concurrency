14e4b60492dda39f97694b405e04bb0f3f4f9623
khannedy <echo.khannedy@gmail.com>
2024-05-27 16:18:07 +0700

    Add new unit tests to main module
    This commit adds four new tests to the main module: `test_join_thread`, `calculate`, `test_sequential`, and `test_parallel`. These tests mainly cover thread creation, thread joining and handling simple parallel and sequential calculations.

--- src/main.rs
+++ src/main.rs
@@ -4,12 +4,13 @@ fn main() {
 
 #[cfg(test)]
 mod tests {
-
     use std::thread;
+    use std::thread::JoinHandle;
     use std::time::Duration;
+    use crate::main;
 
     #[test]
-    fn test_create_thread(){
+    fn test_create_thread() {
         thread::spawn(|| {
             for i in 0..=5 {
                 println!("Counter : {}", i);
@@ -21,4 +22,66 @@ mod tests {
         thread::sleep(Duration::from_secs(7));
     }
 
+    #[test]
+    fn test_join_thread() {
+        let handle: JoinHandle<i32> = thread::spawn(|| {
+            let mut counter = 0;
+            for i in 0..=5 {
+                println!("Counter : {}", i);
+                thread::sleep(Duration::from_secs(1));
+                counter += 1;
+            }
+            return counter;
+        });
+
+        println!("Waiting Handle");
+
+        let result = handle.join();
+        match result {
+            Ok(counter) => println!("Total Counter : {}", counter),
+            Err(error) => println!("Error : {:?}", error)
+        }
+
+        println!("Application done");
+    }
+
+    fn calculate() -> i32 {
+        let mut counter = 0;
+        for i in 0..=5 {
+            println!("Counter : {}", i);
+            thread::sleep(Duration::from_secs(1));
+            counter += 1;
+        }
+        return counter;
+    }
+
+    #[test]
+    fn test_sequential() {
+        let result1 = calculate();
+        let result2 = calculate();
+        println!("Total Counter 1 : {}", result1);
+        println!("Total Counter 2 : {}", result2);
+        println!("Application Finish");
+    }
+
+    #[test]
+    fn test_parallel() {
+        let handle1 = thread::spawn(|| calculate());
+        let handle2 = thread::spawn(|| calculate());
+
+        let result1 = handle1.join();
+        let result2 = handle2.join();
+
+        match result1 {
+            Ok(counter) => { println!("Total Counter 1 : {}", counter) }
+            Err(error) => { println!("Error : {:?}", error) }
+        }
+
+        match result2 {
+            Ok(counter) => { println!("Total Counter 2 : {}", counter) }
+            Err(error) => { println!("Error : {:?}", error) }
+        }
+
+        println!("Application Finish");
+    }
 }
